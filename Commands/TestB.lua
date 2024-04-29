29644cbcf3c829195fb12fc843c1bac355d4d627
Vicnof <76600247+Vicnof@users.noreply.github.com>
2024-04-29 05:06:02 -0700

    Added working birthdays

--- Commands/TestB.lua
+++ Commands/TestB.lua
@@ -28,21 +28,74 @@ if Day < 1 or Day > 31 or Day == nil then
     return
 end
 
-local Birthday = {
+local LocalizedBirthdayTimestamp = os.time{
     ["year"] = tonumber(os.date("%Y")),
     ["month"] = Month,
     ["day"] = Day,
-    ["hour"] = 4
+    ["hour"] = 0,
+    isdst = false
 }
 
-if os.time(Birthday) < os.time() then
-    Birthday["year"] = tonumber(os.date("%Y")) + 1
+local UTCBirthdayTimestamp = os.time(os.date("!*t", LocalizedBirthdayTimestamp))
+local MidnightUTCBirthdayTimestamp = (LocalizedBirthdayTimestamp - math.floor(os.difftime(LocalizedBirthdayTimestamp, UTCBirthdayTimestamp)))
+local Birthday = os.date("!*t", MidnightUTCBirthdayTimestamp)
+
+local OffsetFromUTC = -5
+if os.date("*t", os.time())["isdst"] then
+    Offset = -4
+end
+
+Birthday["hour"] = Birthday["hour"] - Offset
+
+local Prepared = Database:prepare[[
+    INSERT INTO Birthdays(UserID, BirthdayTimestamp)
+	VALUES(?, ?)
+	ON CONFLICT(UserID)
+	DO UPDATE SET BirthdayTimestamp = ?;
+]]
+
+Prepared:reset():bind(Message.author.id, tostring(os.time(Birthday)), tostring(os.time(Birthday))):step()
+
+local Get = Database:exec[[
+    SELECT UserID, BirthdayTimestamp FROM Birthdays
+    ORDER BY CAST (BirthdayTimestamp AS INTEGER) ASC;
+]]
+
+local BirthdayText = ""
+
+for Index, Timestamp in pairs(Get[2]) do
+    BirthdayText = BirthdayText.."🎂 <@"..Get[1][Index]..">: "..os.date("%B %d, %Y", os.time(Birthday)).."\n"
+end
+
+local Days = math.ceil((Get[2][1] - os.time()) / 86400) - 1
+
+if Days == 0 then
+    Message.channel:send {
+    embed = {
+        title = "Broner Birthdays",
+        description = BirthdayText,
+        color = 5019893,
+        fields = {
+            {name = "Next Birthday", value = "It's <@"..Get[1][1]..">'s birthday!", inline = false}
+        },
+        footer = {
+            text = "Pies | built with <3 by EE"
+        }
+    }
+}
+return
 end
 
 Message.channel:send {
-    content = Months[Month].." "..Days[Day]..", "..tostring(tonumber(os.date("%Y") + 1)).."\nor <t:"..os.time(Birthday)..":R>",
-    reference = {
-        message = Message,
-        mention = false
+    embed = {
+        title = "Broner Birthdays",
+        description = BirthdayText,
+        color = 5019893,
+        fields = {
+            {name = "Next Birthday", value = "<@"..Get[1][1]..">, in "..Days.." days", inline = false}
+        },
+        footer = {
+            text = "Pies | built with <3 by EE"
+        }
     }
 }
