88179b129b63bd54c173e4f1862590805ac90656
Vicnof <76600247+Vicnof@users.noreply.github.com>
2024-05-10 21:09:30 -0700

    debugging more

--- Commands/SetBirthday.lua
+++ Commands/SetBirthday.lua
@@ -1,9 +1,9 @@
 local Client, Message, Arguments, Database = ...
-
+p("StartSB")
 if not Database then
     return
 end
-
+p("SB1")
 local Months = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"}
 local Days = {"1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th", "31st"}
 
