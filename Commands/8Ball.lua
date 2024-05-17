7360e5544b39a9df839caa1feef95e490c2f8ee5
Vicnof <76600247+Vicnof@users.noreply.github.com>
2024-05-17 10:07:36 -0700

    Added Magic 8 Ball command

---
+++ Commands/8Ball.lua
@@ -0,0 +1,38 @@
+local Command, Message, Arguments, Timer = ...
+
+local Answers = {
+	"It is certain.",
+	"It is decidedly so.",
+	"Without a doubt.",
+	"Yes, definitely.",
+	"You may rely on it.",
+	"As I see it, yes.",
+	"Most likely.",
+	"Outlook good.",
+	"Yes.",
+	"Signs point to... yes.",
+	"Reply hazy, try again.",
+	"Ask again later.",
+	"Better not tell you now.",
+	"Cannot predict now.",
+	"Concentrate and ask again.",
+	"Don't count on it.",
+	"My reply is no.",
+	"My sources say no.",
+	"Outlook not so good.",
+	"Very doubtful."
+}
+
+Message.channel:send("Thinking...")
+
+Message.channel:broadcastTyping()
+
+Timer.sleep(3000)
+
+Message.channel:send {
+	content = "🎱 "..Answers[math.random(1, #Answers)],
+	reference = {
+		message = Message,
+		mention = true
+	}
+}
