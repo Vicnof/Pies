7de7fbea932288a3d4170d9fcbf675a8f3ae65d9
Vicnof <76600247+Vicnof@users.noreply.github.com>
2024-05-24 16:45:11 -0700

    Added update command

---
+++ Commands/Update.lua
@@ -0,0 +1,13 @@
+local Command, Message, Arguments, Database, Timer = ...
+
+if Message.author.id ~= "240291244306071552" then
+	return
+end
+
+Client:stop()
+
+Timer.sleep(3000)
+
+os.execute("bash Commands/Update.sh")
+
+process:exit()
