0508641fd90e2b2a803756345d51f8e8eb2d7cc0
Vicnof <76600247+Vicnof@users.noreply.github.com>
2024-05-26 23:50:50 -0700

    Updated Testing.lua

--- Commands/Test.lua
+++ Commands/Test.lua
@@ -1,6 +1,3 @@
 local Commands, Message, Arguments = ...
 
-local TestTable = {
-    ["252235505318625281"] = "1724385600",
-    ["240291244306071552"] = "1724310000"
-}
+Message.channel:send("Testing")
