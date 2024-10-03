daa15afef0888713e55f91c64e0dfc658287d3f2
Vicnof <76600247+Vicnof@users.noreply.github.com>
2024-10-02 18:43:31 -0700

    Forced AuroraNow to uncache the image by appending a random query string

--- Commands/AuroraNow.lua
+++ Commands/AuroraNow.lua
@@ -1,12 +1,20 @@
 local Client, Message, Arguments, Database = ...
 
+local Characters = "abcdefghijklmnopqrstuvwxyz1234567890"
+local RandomAppend = ""
+
+for i = 1, 5 do
+    local RandomCharacter = math.random(1, string.len(Characters))
+    RandomAppend = RandomAppend..string.sub(Characters, RandomCharacter, RandomCharacter)
+end
+
 Message.channel:send {
-    content = "This is the lastest 30-minute forecast, representing where the aurora is now. It shows the Northern Hemisphere only.",
+    content = "This is the lastest 30-minute forecast, representing where the aurora is now. It only shows the Northern Hemisphere.",
     embed = {
         title = "From NOAA:",
         url = "https://www.swpc.noaa.gov/communities/aurora-dashboard-experimental",
         image = {
-            url = "https://services.swpc.noaa.gov/images/animations/ovation/north/latest.jpg"
+            url = "https://services.swpc.noaa.gov/images/animations/ovation/north/latest.jpg?r="..RandomAppend
         },
         color = 5523574
     },
