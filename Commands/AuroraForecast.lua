f8920c08935f47cc92ce2fc02ef3858b67a715d4
Vicnof <76600247+Vicnof@users.noreply.github.com>
2024-10-02 18:46:03 -0700

    Forced AuroraForecast to uncache the image by appending a random query string

--- Commands/AuroraForecast.lua
+++ Commands/AuroraForecast.lua
@@ -1,12 +1,20 @@
 local Client, Message, Arguments, Database = ...
 
+local Characters = "abcdefghijklmnopqrstuvwxyz1234567890"
+local RandomAppend = ""
+
+for i = 1, 5 do
+    local RandomCharacter = math.random(1, string.len(Characters))
+    RandomAppend = RandomAppend..string.sub(Characters, RandomCharacter, RandomCharacter)
+end
+
 Message.channel:send {
     content = "This is the latest 24-hour forecast, representing everywhere the aurora is likely to be visible from in the next 24 hours. It shows the Northern Hemisphere only.",
     embed = {
         title = "From NOAA:",
         url = "https://www.swpc.noaa.gov/communities/aurora-dashboard-experimental",
         image = {
-            url = "https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png"
+            url = "https://services.swpc.noaa.gov/experimental/images/aurora_dashboard/tonights_static_viewline_forecast.png?r="..RandomAppend
         },
         color = 5523574
     },
