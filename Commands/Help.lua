80ac8cb48fab78442372f2d2fd3d49fc3f2041ec
Vicnof <76600247+Vicnof@users.noreply.github.com>
2024-10-02 19:47:01 -0700

    Added all commands to help

--- Commands/Help.lua
+++ Commands/Help.lua
@@ -1,7 +1,7 @@
 local Client, Message, Arguments = ...
 
 Message.channel:send {
-    content = "since nordic asked:\n\nvalid prefixes: p, pies, :pie:, <@1225191085202997343>\ncommands: birthdays, birthdayset, pfp, lokloy, nordic, auroranow, auroraforecast, aurora, ping\nADYHB commands: layout, sign, floor, nightmare",
+    content = "since nordic asked:\n\nvalid prefixes: p, pies, :pie:, <@1225191085202997343>\ncommands: birthdays, birthdayset, pfp, lokloy, nordic, auroranow, auroraforecast, aurora, ping, 8ball, color, emoji, flip, and satellite\nADYHB commands: layout, sign, floor, nightmare",
     reference = {
         message = Message,
         mention = false
