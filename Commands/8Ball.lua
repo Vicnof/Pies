4405b1597c82cd45ebd6923794ac320a2f7e280e
Vicnof <76600247+Vicnof@users.noreply.github.com>
2024-12-25 18:33:34 -0800

    Added more options

--- Commands/8Ball.lua
+++ Commands/8Ball.lua
@@ -1,8 +1,34 @@
 local Command, Message, Arguments, Database, Timer = ...
 
 local Answers = {
-	"Absolutely not.",
-	"Of course not."
+	"It is certain.",
+	"It is decidedly so.",
+	"Without a doubt.",
+	"Yes, definitely.",
+	"You may rely on it.",
+	"As I see it, yes.",
+	"Most likely.",
+	"Outlook good.",
+	"Yes.",
+	"Signs point to... yes.",
+	"Sure.",
+	"Definitely.",
+	"Reply hazy, try again.",
+	"Ask again later.",
+	"Better not tell you now.",
+	"Cannot predict now.",
+	"Concentrate and ask again.",
+	"Don't count on it.",
+	"My reply is no.",
+	"My sources say no.",
+	"Outlook not so good.",
+	"Very doubtful.",
+	"Absolutely.",
+	"I guarantee it.",
+	"Of course.",
+	"idk",
+	"nah",
+	"Probably not."
 }
 
 Message.channel:send("Thinking...")
