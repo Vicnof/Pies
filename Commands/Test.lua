e516023604b7d44313133eeaaa08e2f4c3b704b0
Vicnof <76600247+Vicnof@users.noreply.github.com>
2025-04-26 00:34:53 -0700

    Update Test.lua

--- Commands/Test.lua
+++ Commands/Test.lua
@@ -1,6 +1,6 @@
 local Commands, Message, Arguments = ...
 
- local channel = client:getChannel('965444906418049084')
+ local channel = Client:getChannel('965444906418049084')
   local connection = channel:join()
   connection:playFFmpeg('Alice.mp3')
 channel:leave()
