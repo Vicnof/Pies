84ddc78ae1ce414185176f5040f4915e6c91d084
Vicnof <76600247+Vicnof@users.noreply.github.com>
2025-04-29 00:56:13 -0700

    Update Test.lua

--- Commands/Test.lua
+++ Commands/Test.lua
@@ -2,5 +2,4 @@ local Client, Message, Arguments = ...
 
  local channel = Client:getChannel('965444906418049084')
   local connection = channel:join()
-  coroutine.wrap(function()
-  connection:playFFmpeg('Commands/Alice.mp3') end)
+  connection:playFFmpeg('Commands/Alice.mp3')
