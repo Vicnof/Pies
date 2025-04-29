d07bcca459ff9aa71164d325265735bc5c8526f0
Vicnof <76600247+Vicnof@users.noreply.github.com>
2025-04-29 01:14:35 -0700

    Update Test.lua

--- Commands/Test.lua
+++ Commands/Test.lua
@@ -2,5 +2,5 @@ local Client, Message, Arguments = ...
 
  local channel = Client:getChannel('965444906418049084')
   local connection = channel:join()
-  connection:playFFmpeg('\"$(yt-dlp -x -o - \'https://www.youtube.com/watch?v=RRl_n6yj6U8\')\"')
+  connection:playFFmpeg("- $(yt-dlp -x -o - 'https://www.youtube.com/watch?v=RRl_n6yj6U8')")
 channel:leave()
