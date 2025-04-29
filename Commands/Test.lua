311bf03717116ce7188d7eaac43abf2d98a857e6
Vicnof <76600247+Vicnof@users.noreply.github.com>
2025-04-29 03:08:42 -0700

    Update Test.lua

--- Commands/Test.lua
+++ Commands/Test.lua
@@ -3,4 +3,4 @@ local Client, Message, Arguments = ...
  local channel = Client:getChannel('965444906418049084')
   local connection = channel:join()
   connection:playFFmpeg("<(yt-dlp -x -o - 'https://www.youtube.com/watch?v=RRl_n6yj6U8')")
-channel:leave()
+
