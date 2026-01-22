347aa5cacd2ecfcfee983c61bf30efd7bff98dec
Vicnof <76600247+Vicnof@users.noreply.github.com>
2026-01-22 12:34:03 -0800

    Create Budget.lua
    Added command

---
+++ Commands/Budget.lua
@@ -0,0 +1,33 @@
+local Command, Message, Arguments = ...
+
+local Amount = Arguments[2]
+local DAO = 32
+
+local Chase, Wells, America, Robinhood, Kinecta, First
+
+if Amount > (500 + DAO) then
+    Amount = Amount - DAO
+    print(Amount)
+    Chase = math.floor(75 + ((Amount - 500) * 0.10))
+    Wells = math.floor(75 + ((Amount - 500) * 0.10))
+    America = math.floor(25 + ((Amount - 500) * 0.05))
+    Robinhood = math.floor(250 + ((Amount - 500) * 0.65))
+	First = math.floor(75 + ((Amount - 500) * 0.10))
+end
+
+if Amount < (500 + DAO) then
+    Amount = Amount - DAO
+    Chase = math.floor(Amount * 0.15)
+    Wells = math.floor(Amount * 0.15)
+    America = math.floor(Amount * 0.05)
+    Robinhood = math.floor(Amount * 0.50)
+	First = math.floor(Amount * 0.15)
+end
+
+Message.channel:send {
+	content = "Chase: $"..Chase.."\n".."Wells: $"..Wells.."\n".."America: $"..America.."\n".."Robinhood: $"..Robinhood.."\n".."$"..Amount - (Chase + Wells + America + Robinhood).." left over"
+	reference = {
+		message = Message,
+		mention = true
+	}
+}
